d65b1c30ea120c5600b25c7dbf752469de849e2f
devantequick <devantequick94@gmail.com>
2022-04-01 19:12:32 -0400

    spinner is good

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,18 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
+import React from 'react'
+import {render} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Spinner from './Spinner'
+
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+test('Spinner appears', () => {
+  const {queryByText, rerender} = render(<Spinner on={true}/>)
+  expect(queryByText(/please wait.../i)).toBeInTheDocument()
+  rerender(<Spinner on={false}/>)
+  expect(queryByText(/please wait.../i)).not.toBeInTheDocument()
 })
